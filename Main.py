fea78f271c17f22e585703c1ac0e131d0dedf220
Nico <muellernico99@gmail.com>
2018-11-30 18:34:49 +0100

    - erstes Grundgerüst - Wahl des Modus mit Eingabeüberprüfung

---
+++ Main.py
@@ -0,0 +1,25 @@
+def zufall():
+    print("Hier muss die zufall() Methode ausgefüht werden")
+
+def spezifisch():
+    print("Hier muss die spezifisch() Methode ausgeführt werden")
+
+
+def main():
+    print("[Wahl-o-Unimat]\n")
+
+    #   Wahl des Modus mit Überprüfung auf korrekte Eingabe
+    while True:
+        try:
+            i = int(input("Wähle einen Modus: (1) Zufall oder (2) Spezifisch\n"));
+            if i == 1:
+                zufall()
+            elif i == 2:
+                spezifisch()
+            else:
+                i = int("falsche Einfabe")
+            break
+        except ValueError:
+                print("Das war keine gültige Eingabe. Bitte versuche es erneut.")
+
+main()
